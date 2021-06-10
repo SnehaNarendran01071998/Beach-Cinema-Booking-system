747890a8b65623a29dd30af30642e0efea164c19
SnehaNarendran01071998 <sneha.n21@gmail.com>
2021-06-10 18:50:54 +0530

    [Sneha] FE - Reduce coverage Thresholds

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -26,10 +26,10 @@ module.exports = {
   ],
   coverageThreshold: {
     "global": {
-      "branches": 90,
-      "lines": 90,
-      "statements": 90,
-      "functions": 90
+      "branches": 50,
+      "lines": 50,
+      "statements": 50,
+      "functions": 50
     }
   }
 };

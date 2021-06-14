ce56aeb38ee526a9bbd1c63382ceaf6991136e14
SnehaNarendran01071998 <sneha.n21@gmail.com>
2021-06-14 15:30:59 +0530

    [Sneha] FE - Add posr t in webpack.config

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,4 +1,6 @@
 const path = require("path");
+var server_port = 8080 || process.env.PORT || 5000;
+var server_host = "localhost" || "0.0.0.0";
 module.exports = (env) => {
   return {
     mode: env ? "production" : "development",
@@ -17,6 +19,8 @@ module.exports = (env) => {
         https: true,
       },
       historyApiFallback: true,
+      port: server_port,
+      host: server_host,
     },
     resolve: {
       extensions: [

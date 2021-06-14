42e1265f8198ee712d9e5dac6d5ca68635896e7a
SnehaNarendran01071998 <sneha.n21@gmail.com>
2021-06-14 20:09:03 +0530

    [Sneha] FE - Use ports in server.ts

--- frontend/server.ts
+++ frontend/server.ts
@@ -1,12 +1,18 @@
 const express = require("express");
-var cors = require('cors');
-let  app = express();
+var cors = require("cors");
+let app = express();
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(cors());
 app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-    next()
-  })
-app.listen(3000,  () => console.log("Example app listening on port 3000!"));
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+var port = process.env.PORT || 3000 || 5000;
+app.listen(port, "0.0.0.0", function () {
+  console.log("Listening on Port 3000");
+});

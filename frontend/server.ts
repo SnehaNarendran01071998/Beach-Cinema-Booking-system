db94047a13ccd0c345fc81714ff146b8efa5f932
SnehaNarendran01071998 <sneha.n21@gmail.com>
2021-06-15 10:25:42 +0530

    [Sneha] FE - Add the heroku domain to in allow origin

--- frontend/server.ts
+++ frontend/server.ts
@@ -4,11 +4,18 @@ let app = express();
 
 app.use(express.static("public"));
 app.use(cors());
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", '*');
+app.use(function (req, res, next) {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://cinema-booking-backend.herokuapp.com" || "*"
+  );
+
   res.header("Access-Control-Allow-Credentials", true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+  );
   next();
 });
 var port = process.env.PORT || 3000 || 5000;

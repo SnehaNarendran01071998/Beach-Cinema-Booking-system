bb4304860103b944c63f2e44d69cb50a0209559a
SnehaNarendran01071998 <sneha.n21@gmail.com>
2021-06-16 11:33:51 +0530

    [Sneha] FE - install express, update app.use

--- frontend/server.ts
+++ frontend/server.ts
@@ -3,7 +3,7 @@ var cors = require("cors");
 let app = express();
 
 app.use(express.static("public"));
-app.use(cors());
+app.use(cors({ origin: "https://cinema-booking-backend.herokuapp.com"}))
 
 var port = process.env.PORT || 3000 || 5000;
 app.listen(port, "0.0.0.0", function () {

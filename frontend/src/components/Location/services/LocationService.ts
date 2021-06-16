7ae90d101ff3380808e9dce4ae404a8fa6c8d474
SnehaNarendran01071998 <sneha.n21@gmail.com>
2021-06-16 10:36:40 +0530

    [Sneha] FE - CHnage th ebuild command, with main.js and remove urls from Locationservice

--- frontend/src/components/Location/services/LocationService.ts
+++ frontend/src/components/Location/services/LocationService.ts
@@ -3,16 +3,8 @@ import baseURL from "../../../api/base";
 import { LocationResponse } from "../model/LocationResponse";
 
 export default async function getAllLocations() {
-  const herokuapp = "https://cinema-booking-backend.herokuapp.com";
-  const localhost = "http://localhost:9090";
-  const proxy = "https://cryptic-ocean-50781.herokuapp.com/";
-
-  const url =
-    process.env.NODE_ENV === "production" ? `${proxy}${baseURL}` : baseURL;
-  const headerUrl =
-    process.env.NODE_ENV === "production" ? `${herokuapp}` : localhost;
-  const response = await axios.get<LocationResponse[]>(`${url}/location/all`, {
-    headers: { Origin: headerUrl, "Access-Control-Allow-Origin:": "*"},
-  });
+  const response = await axios.get<LocationResponse[]>(
+    `${baseURL}/location/all`
+  );
   return response.data;
 }

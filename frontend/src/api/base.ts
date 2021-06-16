635823483b514a35cbb301b99f007acf7aebfad3
SnehaNarendran01071998 <sneha.n21@gmail.com>
2021-06-16 11:03:12 +0530

    [Sneha] FE - Add protocol in the base heroku url

--- frontend/src/api/base.ts
+++ frontend/src/api/base.ts
@@ -1,5 +1,5 @@
 const proxy = "https://cryptic-ocean-50781.herokuapp.com/";
-const herokuapp = "cinema-booking-backend.herokuapp.com";
+const herokuapp = "https://cinema-booking-backend.herokuapp.com";
 const localhost = "http://localhost:9090";
 const baseURL =
   process.env.NODE_ENV === "production" ? `${herokuapp}` : localhost;

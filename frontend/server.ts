06745e5ec3ffd0d09164684adb45527ffcbacd73
SnehaNarendran01071998 <sneha.n21@gmail.com>
2021-06-16 11:17:33 +0530

    [Sneha] FE - Remove hearders settings from server

--- frontend/server.ts
+++ frontend/server.ts
@@ -4,17 +4,7 @@ let app = express();
 
 app.use(express.static("public"));
 app.use(cors());
-app.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "http://cinema-booking-backend.herokuapp.com"
-  );
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-})
+
 var port = process.env.PORT || 3000 || 5000;
 app.listen(port, "0.0.0.0", function () {
   console.log(`Listening on Port ${port}`);
